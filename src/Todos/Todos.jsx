fb83642d5bef1ef5800c6f6e0fb26d57703cd8e6
Manu-999 <manuelbalbas@gmail.com>
2019-04-17 12:06:20 +0200

    structure created

---
+++ src/Todos/Todos.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Todos.scss';
+
+class Todos extends React.Component {
+    render() {
+        return (
+            <div className="todos">
+                <div className="addTask">
+                </div>
+                <div className="taskList">
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Todos;
